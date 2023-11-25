0c080bd0e3a483573a1c858dbfdf453ea0253955
toychip <dlawnsgud427@naver.com>
2023-11-26 07:37:09 +0900

    feat: 수락시 요청된 것 수정 (#32)

--- src/main/java/com/example/hackathon/domain/friend/repository/FriendRepository.java
+++ src/main/java/com/example/hackathon/domain/friend/repository/FriendRepository.java
@@ -2,6 +2,15 @@ package com.example.hackathon.domain.friend.repository;
 
 import com.example.hackathon.domain.friend.domain.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
+    @Query("SELECT f FROM Friend f "
+            + "WHERE f.fromUserEmail = :fromUserEmail "
+            + "AND f.toUserEmail = :toUserEmail")
+    Friend getFriendRequest(
+            @Param("fromUserEmail") String fromUserEmail,
+            @Param("toUserEmail") String toUserEmail);
+
 }
