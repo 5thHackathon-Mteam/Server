dbaf0d4a2dda6985fb1a69b16cf7bd0966aad50e
toychip <dlawnsgud427@naver.com>
2023-11-25 20:09:54 +0900

    feat: Feed 양방향 매핑, 카테고리 컬럼 추가 (#1)

--- src/main/java/com/example/hackathon/domain/feed/domain/Category.java
+++ src/main/java/com/example/hackathon/domain/feed/domain/Category.java
@@ -4,11 +4,13 @@ public enum Category {
     DAILY, // 일상
     TRAVEL, //여행
     FAMILY, // 가족
+    FRIEND,
     COUPLE, // 연인
     FOOD, // 음식
     SPACE, // 공간
     EXHIBITION, // 전시
     FASHION, // 패션
     EXERCISE, // 운동
-    CULTURE; // 문화
+    CULTURE, // 문화
+    NONE; // DEFAULT
 }
--- src/main/java/com/example/hackathon/domain/feed/domain/Feed.java
+++ src/main/java/com/example/hackathon/domain/feed/domain/Feed.java
@@ -1,12 +1,16 @@
 package com.example.hackathon.domain.feed.domain;
 
 import com.example.hackathon.domain.base.BaseEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -28,5 +32,9 @@ public class Feed extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     private Category category;
+
+    @OneToMany(mappedBy = "feed", cascade = CascadeType.REMOVE)
+    @Builder.Default
+    private List<FeedImage> imageUrlList = new ArrayList<>();
 
 }
